6d96b6f095b138b197f7737d8f94c6fcd48cb011
Ahmed Al Haddad <ahmed.hdd@gmail.com>
2018-08-06 16:43:43 +0300

    create simple architecture

---
+++ src/factory.js
@@ -0,0 +1,38 @@
+class Factory {
+  
+  constructor(plugins) {
+    this.plugins = plugins;
+  }
+
+  init = () => {
+    console.log("%c[Figgins] Initilizing...", "color: green");
+
+    const plugins = this.plugins;
+    const keys = Object.keys(plugins);
+
+    if (keys.length === 1 && keys[0] === "default") {
+      console.log("%c[Figgins] No plugins found.", "color: green");
+    } else {
+      console.log(keys);
+
+      keys.map((key, index) => {
+        this.setupPlugin(plugins[key]);
+
+        if (index === keys.length - 1) {
+          this.didInitilize();
+        }
+      });
+    }
+  }
+  
+  didInitilize = () => {
+    console.log("%c[Figgins] All set!", "color: green");
+  }
+
+  setupPlugin = (plugin) => {
+    plugin.setup();
+    console.log(`%c[Figgins] Added ${plugin.name} plugin`, "color: green");
+  }
+}
+
+export default Factory;
---
+++ src/index.js
@@ -0,0 +1,6 @@
+import Factory from './factory';
+import * as figgins from "./figgins";
+
+const factory = new Factory(figgins);
+
+factory.init();
