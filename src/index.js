f81111cacc1b0d96fade7761e29b0fccfe22763f
Ahmed Al Haddad <ahmed.hdd@gmail.com>
2018-08-07 14:14:48 +0300

    add chrome extension

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import Factory from './factory';
 import * as figments from "./figments";
+import './assets/figments.css';
 
 const factory = new Factory(figments);
 
-factory.init();
+factory.run();
