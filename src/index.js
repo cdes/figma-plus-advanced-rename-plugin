7b626f719e146ce833b3e7eb5556be5038151a48
Ahmed Al Haddad <ahmed.hdd@gmail.com>
2018-11-16 04:15:18 +0300

    Restructure for plugins manager

--- src/index.js
+++ src/index.js
@@ -1,31 +1,3 @@
-import 'vex-js/dist/css/vex.css';
-import 'vex-js/dist/css/vex-theme-plain.css';
-import './assets/figments.css';
+import batchRename from 'batch-rename';
 
-import Factory from './factory';
-import * as figments from "./figments";
-
-const factory = new Factory(figments);
-
-window.FigmentsFactory = factory;
-window.FigmentsFactory.run();
-
-(function(history){
-  var pushState = history.pushState;
-  history.pushState = function(state) {
-      if (typeof history.onpushstate == "function") {
-          history.onpushstate({state: state});
-      }
-
-      if(typeof window.FigmentsFactory === "undefined") {
-        window.FigmentsFactory = factory;
-      }
-
-      if(!window.FigmentsFactory.isInjecting) {
-        console.log("🔌 Reinjecting");
-        window.FigmentsFactory.run();
-      }
-
-      return pushState.apply(history, arguments);
-  }
-})(window.history);
+export default batchRename;
